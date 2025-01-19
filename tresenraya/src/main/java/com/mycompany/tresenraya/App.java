8399c1ae13d4ae356e79ce3fd1675cda058255df
paulettemal <136779147+paulettemal@users.noreply.github.com>
2025-01-19 02:38:07 -0500

    Update App.java

--- tresenraya/src/main/java/com/mycompany/tresenraya/App.java
+++ tresenraya/src/main/java/com/mycompany/tresenraya/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 /**
  * JavaFX App
@@ -16,17 +17,18 @@ public class App extends Application {
     private static Scene scene;
     
     public static String pathFiles="src/main/resources/files/";
-    //publics static String pathImg="src/main/resources/images/";
     
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmLoader=new FXMLLoader(App.class.getResource("PantallaInicial.fxml"));
         
         Parent root=fxmLoader.load();
+        
         scene=new Scene(root,600,500);
         stage.setScene(scene);
         stage.setTitle("TIC TAC TOE");
         stage.getIcons().add(new Image(App.class.getResourceAsStream("/images/icon.png")));
+
         stage.setResizable(false);
         stage.show();
         
